b03ee91e030be539fe28a4946bb4b783a303a16f
Josh Arntz <jarntz12@gmail.com>
2017-04-18 13:40:20 -0500

    Added do and try for audio recorders and catches

--- Soundboard/SoundViewController.swift
+++ Soundboard/SoundViewController.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
-
+    
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var textField: UITextField!
+    
+    var audioRecorder : AVAudioRecorder?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpRecorder()
+        
+        
+    }
+    
+    
+    func setUpRecorder() {
+        
+        //Create an audio session
+        do {
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            
+            //Create URL for the audio file
+            
+            //Create Settings for the audio recorder
+            
+            //Create AudioRecorder Object
+            audioRecorder = AVAudioRecorder(url: , settings: <#T##[String : Any]#>)
+        } catch let error as NSError {
+            print(error)
+        } catch {}
     }
     
     @IBAction func playTapped(_ sender: Any) {
@@ -35,15 +62,15 @@ class SoundViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
